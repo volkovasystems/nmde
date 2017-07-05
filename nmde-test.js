1c466150eedcc995453ed44f6ff2ba49582cbd4e
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-05 13:12:42 +0800

    Updated server-side test.

--- nmde-test.js
+++ nmde-test.js
@@ -2,12 +2,23 @@
 const assert = require( "assert" );
 const nmde = require( "./nmde.js" );
 
+assert.equal( nmde( { "name": "yeah" } ), "yeah", "should have name 'yeah'" );
+
 assert.equal( nmde( function hello( ){ } ), "hello", "should have name 'hello'" );
 
-assert.equal( nmde( { } ), "", "should be empty name" );
+assert.equal( nmde( Error ), "Error", "should return 'Error'" );
 
-assert.equal( nmde( ), "", "should be empty name" );
+assert.equal( nmde( ) == "", true, "should return true" );
 
-assert.equal( nmde( { "name": "yeah" } ), "yeah", "should have name 'yeah'" );
+assert.equal( nmde( "test" ) == "", true, "should return true" );
+
+assert.equal( nmde( { } ) == "", true, "should return true" );
+
+assert.equal( nmde( function( ){ } ) == "", true, "should return true" );
+
+assert.equal( nmde( ( ) => { } ) == "", true, "should return true" );
+
+assert.equal( nmde( ( entity ) => ( typeof entity == "string" ) ) == "",
+				true, "should return true" );
 
 console.log( "ok" );
